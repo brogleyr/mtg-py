a29e32c3962624ccedf9be8c171a0730dc988271
brogleyr <ryan.brogley@gmail.com>
2022-10-21 17:18:47 -0400

    100.3

--- src/card.py
+++ src/card.py
@@ -2,6 +2,11 @@
 class Card:
     name = ''
     card_type = ''
+
+    # 100.3
+    special_designation = None
+    nontraditional = False
+
     def __init__(self, name, card_type=''):
         self.name = name
         self.type = card_type
--- src/small_items.py
+++ src/small_items.py
@@ -1,5 +1,17 @@
+# 100.2
 class Token:
     pass
 
+# 100.2
 class Counter:
     pass
+
+# 100.3
+class Coin:
+    pass
+
+# 100.3
+class Die:
+    def __init__(self, traditional):
+        self.traditional = traditional
+
